9f7008123e9337e281ff9ee3e7e8873befc75542
Antropov Anton <antropov.a.g@gmail.com>
2022-07-26 21:51:20 +0400

    Change to index router: signin/up/out and auth extracted from users

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,9 @@
 const userRouter = require('express').Router();
 const { Joi, celebrate } = require('celebrate');
-const auth = require('../middlewares/auth');
 const {
   updateUserInfo,
   getCurrentUserInfo,
-  userLogin,
-  userSignup,
-  userSignOut,
 } = require('../controllers/users');
-
-userRouter.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), userLogin);
-
-userRouter.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), userSignup);
-
-userRouter.use(auth);
 
 userRouter.get('/users/me', getCurrentUserInfo);
 
@@ -34,7 +13,5 @@ userRouter.patch('/users/me', celebrate({
     email: Joi.string().required().email(),
   }),
 }), updateUserInfo);
-
-userRouter.post('/signout', userSignOut);
 
 module.exports = userRouter;
